b6fb04bf16d1939447d912c0a98087a18c4a349e
Pooya Hatami <pooya@node-1w7jr9qrok3i9p5vcbqtrebd6.ipv6.telus.net>
2018-03-13 15:44:37 -0700

    Add icon and extra 3d shape

--- IOS-Swift-ARKit3DBB8/ViewController.swift
+++ IOS-Swift-ARKit3DBB8/ViewController.swift
@@ -22,22 +22,67 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         let sphere =  SCNSphere(radius: 0.2)
         let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named: "art.scnassets/2k_mars.jpg")
+        material.diffuse.contents = UIImage(named: "art.scnassets/2k_sun.jpg")
         sphere.materials = [material]
+        
+        let sphere01 =  SCNSphere(radius: 0.2)
+        let material01 = SCNMaterial()
+        material01.diffuse.contents = UIImage(named: "art.scnassets/2k_earth_daymap.jpg")
+        sphere01.materials = [material01]
+
+        
+        let sphere02 =  SCNSphere(radius: 0.2)
+        let material02 = SCNMaterial()
+        material02.diffuse.contents = UIImage(named: "art.scnassets/2k_mars.jpg")
+        sphere02.materials = [material02]
+
+        
+        let sphere03 =  SCNSphere(radius: 0.2)
+        let material03 = SCNMaterial()
+        material03.diffuse.contents = UIImage(named: "art.scnassets/2k_venus_surface.jpg")
+        sphere03.materials = [material03]
+
+        
+        let sphere04 =  SCNSphere(radius: 0.2)
+        let material04 = SCNMaterial()
+        material04.diffuse.contents = UIImage(named: "art.scnassets/2k_neptune.jpg")
+        sphere04.materials = [material04]
+
         
         // creating a node object
         
         let node = SCNNode()
         node.position = SCNVector3(x: 0 , y: 0.1 , z: -0.5)
+
+        let node01 = SCNNode()
+        node01.position = SCNVector3(x: -0.5 , y: -0.5 , z: -1)
+
+        let node02 = SCNNode()
+        node02.position = SCNVector3(x: -1 , y: -1 , z: 2)
+
+        let node03 = SCNNode()
+        node03.position = SCNVector3(x: -1.5 , y: -1.5 , z: 0.5)
+
+        let node04 = SCNNode()
+        node04.position = SCNVector3(x: 2 , y: 2 , z: 2)
+
         
         // placing our moon to the postion / node we creat
         
         node.geometry = sphere
-        
+        node01.geometry = sphere01
+        node02.geometry = sphere02
+        node03.geometry = sphere03
+        node04.geometry = sphere04
+
         // adding moon to the scence
         
         sceneView.scene.rootNode.addChildNode(node)
-        
+        sceneView.scene.rootNode.addChildNode(node01)
+        sceneView.scene.rootNode.addChildNode(node02)
+        sceneView.scene.rootNode.addChildNode(node03)
+        sceneView.scene.rootNode.addChildNode(node04)
+
         // project light on the moon and make it look like a 3d object
         
         sceneView.autoenablesDefaultLighting = true
